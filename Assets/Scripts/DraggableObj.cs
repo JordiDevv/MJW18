5057d2aa6c090989a5b6292e0d309e5bd9f5953f
Carlos <kr2moran99@gmail.com>
2024-07-28 02:21:08 +0200

    feat: prepare complements before pizza

--- Assets/Scripts/DraggableObj.cs
+++ Assets/Scripts/DraggableObj.cs
@@ -12,8 +12,7 @@ public enum tags
 
 public class DraggableObj : MonoBehaviour
 {
-    [SerializeField]
-    public tags nameTag; 
+    [SerializeField] public tags nameTag;
 
     public string itemID; //identificador del objeto
     public bool pizzaTopping;
@@ -22,23 +21,22 @@ public class DraggableObj : MonoBehaviour
     private Vector3 dragOffset;
     private Camera cam;
 
-    [SerializeField] 
-    private float speed = 10f;
-    [SerializeField]
-    private bool itemBeingDragged;
+    [SerializeField] private float speed = 10f;
+    [SerializeField] private bool itemBeingDragged;
     private bool inDropArea;
-    
-    [SerializeField]
-    private GameObject target;
+
+    [SerializeField] private GameObject target;
 
     private Vector2 initialPos;
 
 
-    private void Awake() {
-        cam = Camera.main;  //reference to main camera 
+    private void Awake()
+    {
+        cam = Camera.main; //reference to main camera 
     }
 
-    private void OnMouseDown() {
+    private void OnMouseDown()
+    {
         dragOffset = transform.position - GetMousePosition();
         itemBeingDragged = true;
 
@@ -50,47 +48,46 @@ public class DraggableObj : MonoBehaviour
     /// <summary>
     /// Spawnea el objeto que se coge
     /// </summary>
-    public void SpawnDraggableObject() {
-        Instantiate(gameObject, new Vector3(initialPos.x,initialPos.y,0f), Quaternion.identity);
+    public void SpawnDraggableObject()
+    {
+        Instantiate(gameObject, new Vector3(initialPos.x, initialPos.y, 0f), Quaternion.identity);
     }
-
 
 
     /// <summary>
     /// Acciones mientras se hace drag
     /// </summary>
-    private void OnMouseDrag() {
-
-        if (itemBeingDragged) {
+    private void OnMouseDrag()
+    {
+        if (itemBeingDragged)
+        {
             //Move the item towards the position of the mouse
             transform.position = Vector3.MoveTowards(transform.position, GetMousePosition() + dragOffset, speed);
-            
         }
-        
-
     }
 
     /// <summary>
     /// Acciones al levantar el click del raton
     /// </summary>
-    private void OnMouseUp() {
+    private void OnMouseUp()
+    {
         itemBeingDragged = false;
-        if (inDropArea && GameManager.Instance.menuItemsCounter < 4) {
+        if (inDropArea)
+        {
             //compruebo si forma parte de los ingredientes de la pizza
-            if (pizzaTopping) {
+            if (pizzaTopping)
+            {
                 Pizza pizza = target.GetComponent<Pizza>();
                 pizza.UpdateSprite(itemID);
                 Destroy(gameObject, 0.2f);
-            } else {
-                FindAvailablePosition();
             }
-            
-           
+            else if (GameManager.Instance.menuItemsCounter < 4) FindAvailablePosition();
+
+
             gameObject.GetComponent<Collider2D>().enabled = false;
-
-
-
-        } else {
+        }
+        else
+        {
             Destroy(gameObject);
         }
     }
@@ -99,65 +96,60 @@ public class DraggableObj : MonoBehaviour
     /// Obtiene la position del raton
     /// </summary>
     /// <returns></returns>
-    Vector3 GetMousePosition() {
-
+    Vector3 GetMousePosition()
+    {
         Vector3 mousePosition = cam.ScreenToWorldPoint(Input.mousePosition);
-        mousePosition.z = 0;   //no haria falta, pero por si acaso
+        mousePosition.z = 0; //no haria falta, pero por si acaso
         return mousePosition;
-
     }
 
-    private void OnTriggerEnter2D(Collider2D collision) {
-        if (collision.tag == nameTag.ToString()) {
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == nameTag.ToString())
+        {
             inDropArea = true;
             target = collision.gameObject;
-            
+
             Debug.Log(target);
         }
-
-
     }
 
-    private void OnTriggerExit2D(Collider2D collision) {
-        if (collision.tag == nameTag.ToString()) {
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag == nameTag.ToString())
+        {
             inDropArea = false;
             target = null;
         }
-
-
     }
 
 
-    private void FindAvailablePosition() {
-        if (target != null) {
+    private void FindAvailablePosition()
+    {
+        if (target != null)
+        {
             dropAreaPositions = target.GetComponentsInChildren<Transform>();
             float distance = 100f;
             Vector3 newPos = Vector3.zero;
-            for (int i = 0; i < dropAreaPositions.Length; i++) {
-                if (dropAreaPositions[i].tag == "Pos" && dropAreaPositions[i] != null && dropAreaPositions[i].childCount <= 0) {
-
-                    
+            for (int i = 0; i < dropAreaPositions.Length; i++)
+            {
+                if (dropAreaPositions[i].tag == "Pos" && dropAreaPositions[i] != null &&
+                    dropAreaPositions[i].childCount <= 0)
+                {
                     float newDistance = Vector2.Distance(dropAreaPositions[i].transform.position, GetMousePosition());
-                    if (newDistance < distance) {
+                    if (newDistance < distance)
+                    {
                         newPos = dropAreaPositions[i].transform.position;
                         gameObject.transform.parent = dropAreaPositions[i].transform;
-                        
                     }
+
                     Debug.Log("Hay posicion disponible");
-                   
                 }
             }
+
             transform.position = newPos;
-           GameManager.Instance.menu.Add(itemID);
-           GameManager.Instance.menuItemsCounter++; //aumentamos el contador de objetos
+            GameManager.Instance.menu.Add(itemID);
+            GameManager.Instance.menuItemsCounter++; //aumentamos el contador de objetos
         }
-
-
-
     }
 }
-
-
-  
-
-
