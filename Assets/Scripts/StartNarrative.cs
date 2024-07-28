7c131c389bf295f8770aebef3506b8fd0efb557d
Anzuniu <158233603+Anzuniu@users.noreply.github.com>
2024-07-28 11:23:46 +0200

    Reapply "Ajustes Time Intro"
    This reverts commit fddfb196ddde98b75a1497f8cc125ebd76569cfb.

--- Assets/Scripts/StartNarrative.cs
+++ Assets/Scripts/StartNarrative.cs
@@ -8,9 +8,15 @@ public class StartNarrative : MonoBehaviour
     [SerializeField] private GameObject narrativePanel;
     [SerializeField] private GameObject firstChat;
     [SerializeField] private GameObject secondChat;
+    [SerializeField] private GameObject bossImage;
+    [SerializeField] private GameObject meImage;
     public string sceneName;
     public int timeStart;
-    public int timeChat;
+    public int timeChat1In;
+    public int timeChat2In;
+    public int timeChat1Out;
+    public int timeChat2Out;
+    public int timeMeImageIn;
     public int timeOut;
 
     // Start is called before the first frame update
@@ -29,13 +35,19 @@ public class StartNarrative : MonoBehaviour
     {
         narrativePanel.SetActive(true);
         yield return new WaitForSeconds(timeStart);
+        bossImage.SetActive(true);
+        yield return new WaitForSeconds(timeChat1In);
         firstChat.SetActive(true);
-        yield return new WaitForSeconds(timeChat);
+        yield return new WaitForSeconds(timeMeImageIn);
+        meImage.SetActive(true);
+        yield return new WaitForSeconds(timeChat2In);
         secondChat.SetActive(true);
-        yield return new WaitForSeconds(timeChat + 4);
+        yield return new WaitForSeconds(timeChat1Out);
         firstChat.SetActive(false);
-        yield return new WaitForSeconds(timeChat - 2);
+        bossImage.SetActive(false);
+        yield return new WaitForSeconds(timeChat2Out);
         secondChat.SetActive(false);
+        meImage.SetActive(false);
         yield return new WaitForSeconds(timeOut);
         SceneManager.LoadScene(sceneName);
     }
